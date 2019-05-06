ec8e751f323724dd028c18ce21b6e2faf0fb4961
yakmax <yakmax@gmail.com>
2019-05-07 00:35:21 +0500

    1

---
+++ my.js
@@ -0,0 +1,38 @@
+//Случайное число
+function func_01_(){
+  var diap = 9; 	
+  if (confirm("Хотите сгенерировать случайное число ?")) { 
+    x = Math.floor(Math.random() * diap); 
+	alert("Число = " + x) 
+  } 
+  else { alert("Ну пока ..."); 
+  } 
+}
+
+//1. поиска суммы чисел от 1 до N
+function func_01(){ 
+  var n = +prompt("1. поиска суммы чисел от 1 до N, N= ?");
+  var sum = 0 ;
+  for (i=1; i<=n; i++){ 
+    sum += i; 
+  }
+  alert("Число = " + sum) ;
+}
+
+function func_02(){
+  var n = +prompt("2. Поиск суммы кубов нечётных чисел в диапазоне от 1 до N, N= ?");
+  var sum = 0 ;
+   for (i=1; i<=n; i=i+2){ 
+    sum += i*i*i; 
+  }
+  alert("Число = " + sum) ;
+}
+
+function func_03(){
+  var n = +prompt("3. поиска количества нечётных цифр в целом числе N, N= ?");
+  var sum = 0;
+   for (i=1; i<=n; i=i+2){ 
+    sum += i*i*i; 
+  }
+  alert("Число = " + sum) ;
+}
