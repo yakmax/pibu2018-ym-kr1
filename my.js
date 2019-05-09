b63466fcfc4d702d3cc1a0123390ef6a156e8543
yakmax <yakmax@gmail.com>
2019-05-09 06:12:33 +0500

    20190509

--- my.js
+++ my.js
@@ -38,3 +38,16 @@ function func_03(){
   }
   alert("Число = " + count);
 }
+
+function func_04(){
+  var n = +prompt("Поиск минимальной цифры в целом числе N., N= ?");
+  n=Math.abs(n);
+  var m = 9;
+  //if (n%10<m) m=n%10
+  while (n>0) {
+    if (n%10<m) m=n%10;
+    n=Math.trunc(n/10);  
+  }
+  alert("Число = " + m);
+
+}
